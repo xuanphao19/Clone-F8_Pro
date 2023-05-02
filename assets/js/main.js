e1b72526177e2604829921cc4bfceff4fd0a1b85
Xuan Phao <nguyenthanhhoa075@gmail.com>
2023-05-02 12:43:09 +0700

    Welcome to f8

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,21 +33,8 @@
   }
   const glightbox = GLightbox({
     selector: ".glightbox",
-    //   Multiple data attributes / You can use the options as separated data attributes
-    // <a
-    //   class="glightbox || 1 - n"            Lớp. (Bộ chọn CSS = true)
-    //   href="large.jpg"                      tiêu đề
-    //   data-title="My title"                 thay thế
-    //   data-description="description here"   Sự miêu tả
-    //   data-desc-position="right"            mô tảVị trí
-    //   data-type="image"                     kiểu
-    //   data-effect="fade"                    tác dụng
-    //   data-width="900px"                    chiều rộng
-    //   data-height="auto"                    chiều cao
-    //   data-zoomable="true"                  có thể phóng to
-    //   data-draggable="true"                 có thể kéo được
-    // ></a>
   });
+
   const portfolioLightbox = GLightbox({
     selector: ".Xuan_Phao",
   });
@@ -75,29 +62,6 @@
     selector: ".phaoThu",
   });
 
-  // new Swiper(".recommend_slider", {
-  //   // direction: "mặc định horizontal || auto; 'vertical === Thẳng đứng'",
-  //   /*    effect: "fade",
-  //   fadeEffect: { crossFade: true, }, Hiệu ứng mờ */
-  //   // breakpointBase: "container",
-  //   // centeredSlides: true,
-  //   slidesPerView: 4,
-  //   spaceBetween: 15,
-  //   centeredSlides: false,
-  //   speed: 4500,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 1500,
-  //     disableOnInteraction: false,
-  //   },
-  //   breakpoints: {
-  //     // 480: {//   slidesPerView: 2, //   spaceBetween: 10, // },
-  //     // 640: { //   slidesPerView: 3, //   spaceBetween: 10, // },
-  //     // // when window width is >= 820px Ok cái này hay!
-  //     // 820: { //   slidesPerView: 4, //   spaceBetween: 15, // },
-  //   },
-  // });
-
   window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
@@ -106,4 +70,12 @@
       mirror: false,
     });
   });
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      responsiveVoice.speak("Chào mừng bạn đến với F8, fullstack chấm edu chấm vn", "Vietnamese Female");
+    },
+    false,
+  );
 })();
